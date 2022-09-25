5501bfe8d58d434fffe18c4e259a85ea152cf8b0
Cole <crobin27@calpoly.edu>
2022-09-24 22:46:47 -0700

    Delete buttonworking

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,40 +1,43 @@
-import React from 'react'
-import Table from './Table'
-
-
-const characters = [
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-];
-
+import React, { useState } from 'react';
+import Table from './Table';
 
 function MyApp() {
+    const [characters, setCharacters] = useState([
+        {
+            name: 'Charlie',
+            job: 'Janitor',
+        },
+        {
+            name: 'Mac',
+            job: 'Bouncer',
+        },
+        {
+            name: 'Dee',
+            job: 'Aspiring Actress',
+        },
+        {
+            name: 'Dennis',
+            job: 'Bartender',
+        },
+
+    ]);
+
+    /*The filter function works similar to a for loop, the first argument is the value of the
+    current character being iterated over (not used in this case), the second parameter holds the
+    index value of the current character being iterated over, which is why we are comparing i !== index*/
+
+    function removeOneCharacter(index) {
+        const updated = characters.filter((character, i) => {
+            return i !== index
+        });
+        setCharacters(updated);
+    }
+
     return (
         <div className="container">
-          <Table characterData={characters} />
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
         </div>
-      )
+    );
 }
-
-
-
-
-  
-  
-
 
 export default MyApp;
