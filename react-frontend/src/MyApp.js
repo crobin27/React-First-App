da45b5595cd4ab1ada2fac0efd905ef7217d3b30
Cole <crobin27@calpoly.edu>
2022-09-24 23:04:39 -0700

    Added Form.js

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,27 +1,9 @@
 import React, { useState } from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: 'Charlie',
-            job: 'Janitor',
-        },
-        {
-            name: 'Mac',
-            job: 'Bouncer',
-        },
-        {
-            name: 'Dee',
-            job: 'Aspiring Actress',
-        },
-        {
-            name: 'Dennis',
-            job: 'Bartender',
-        },
-
-    ]);
-
+    const [characters, setCharacters] = useState([]);
     /*The filter function works similar to a for loop, the first argument is the value of the
     current character being iterated over (not used in this case), the second parameter holds the
     index value of the current character being iterated over, which is why we are comparing i !== index*/
@@ -36,8 +18,11 @@ function MyApp() {
     return (
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form />
         </div>
     );
+
+
 }
 
 export default MyApp;
